8153ffeaa479d879af0d0b588ec5aa31b6811348
Valeria Osorio <valeria.osoriou@gmail.com>
2020-02-24 03:28:39 +1300

    Key working perfect from json

--- js/script.js
+++ js/script.js
@@ -7,43 +7,50 @@ $(document).ready(function(){
 	console.log(myKey[0]);
 	myKey = myKey[0].key;
 	console.log(myKey);
-});
-
-// //DROPDOWN MENUS FOR DIFFERENT CHOICES STORED IN HTML LINKS
-// document.getElementById('submit').addEventListener('click', function(){
-//     choice = document.getElementById('choice').value;
-//     console.log(choice);
-//     displayData(choice);//actual parameter
-// });
-
-//FUNCTION TO DISPLAY CHOICES
-// function displayData(choice){
-//AJAX
-$.ajax({
-	url : `http://newsapi.org/v2/top-headlines?sources=associated-press&apiKey=${myKey}`,
-	type :'GET',
-	data :'json',
-	success:function(data){
-			console.log(data);
-
-			var i;
-    		for (i=0; i<data.length; i++){
-		    	document.getElementById('result').innerHTML;  
-		    	// +=
-		    	// '<div class="col col-sm-6 col-md-4 col-lg-3 border border-primary">' +
-		    	// '<h1>' + data[i].status  + '</h1>' +
-		    	// '<h2>' + data[i].articles[i].author + '</h2>' ;
-		    	// // '<h3>' + data[i].email + '<h3>' +
-		    	// // '<img class="img-thumbnail" src="' + data[i].avatar + '" alt="Avatar">' +    
-		    	// // '</div>';
-		    }
 
 
-	},//end data
-	error:function(){
-			console.log('error');
-	}//end error
-});//end of Ajax
+	//READING USER CHOICE
+	var category;
+	var country;
+	var source;
 
-// }; //function display data
+	document.getElementById('submit').addEventListener('click', function(){
+	  category= document.getElementById('categories').value;
+	  country= document.getElementById('countries').value;
+	  source= document.getElementById('sources').value;
+	  console.log(category, country, source);
+	  displayData(category, country, source);
+	}); //reading user choice
 
+	//DISPLAY DATA FUNCTIONRIGHT BEFORE AJAX, you can name the variables as you want ep/si
+	function displayData(ca, co, so){
+	//AJAX METHOD
+	$.ajax({
+		url : `http://newsapi.org/v2/top-headlines?country=${co}&category=${ca}&apiKey=${myKey}`,
+		type :'GET',
+		data :'json',
+		success:function(data){
+				console.log(data);
+
+				var i;
+	    		for (i=0; i<data.length; i++){
+			    	document.getElementById('result').innerHTML;  
+			    	// +=
+			    	// '<div class="col col-sm-6 col-md-4 col-lg-3 border border-primary">' +
+			    	// '<h1>' + data[i].status  + '</h1>' +
+			    	// '<h2>' + data[i].articles[i].author + '</h2>' ;
+			    	// // '<h3>' + data[i].email + '<h3>' +
+			    	// // '<img class="img-thumbnail" src="' + data[i].avatar + '" alt="Avatar">' +    
+			    	// // '</div>';
+			    }
+
+
+		},//end data
+		error:function(){
+				console.log('error');
+		}//end error
+	});//end of Ajax
+
+	}; //end function display data
+
+});//end of ApiKey
